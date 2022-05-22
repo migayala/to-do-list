0e9696c82c3e0cf97cb8ce33995fc3c666f6e83d
migayala <mayalamontoya@gmail.com>
2022-05-22 15:13:54 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [newTodo, setNewToDo] = useState("");
+  const [todos, setTodos] = useState([]);
+  
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={(event) =>{
+        handleNewTodoSubmit(event);
+      }}>
+        <input type="text"/>
+      </form>
     </div>
   );
 }
